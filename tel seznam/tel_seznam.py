740689c51e4a19375bffbd0b399cd61eb40d050d
CisarT <120019524+CisarT@users.noreply.github.com>
2023-03-28 20:00:21 +0200

    uprava barev a voleb menu

--- tel seznam/tel_seznam.py
+++ tel seznam/tel_seznam.py
@@ -14,11 +14,18 @@ import os.path #importovaná knihovna vnitřní(build-in)
 # Vypsání ASCII artu
 #print(asciiObr)
 
+# definice konstant pro barvy
+RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+YEL = "\033[1;33m"
+BLUE = "\033[1;34m"
+RESET = "\033[0m"
+
 seznam = {}
 
 # Načtení existujícího seznamu ze souboru (pokud existuje)
 if os.path.isfile("telefonni_seznam.py"):
-    with open("telefonni_seznam.py", "r") as f:
+    with open( "telefonni_seznam.py", "r") as f:
         seznam = eval(f.read())
 
 # Funkce pro výpis seznamu
@@ -39,7 +46,7 @@ def pridej_kontakt():
             f.write(str(seznam))
         print("Kontakt byl přidán.")
     else:
-        print("Telefoní číslo má málo čísel")
+        print(RED + "Telefoní číslo má málo čísel.Telefoní kontakt nebyl vytvořen!!!!" + RESET,)
 
 # Funkce pro vyhledání kontaktu podle jména
 def najdi_kontakt():
@@ -51,13 +58,13 @@ def najdi_kontakt():
 
 # Hlavní cyklus programu
 while True:
-    print("\nCo chceš udělat?")
-    print("1 - Vypsat seznam kontaktů")
-    print("2 - Přidat nový kontakt")
-    print("3 - Najít kontakt")
-    print("4 - Konec programu")
+    print(GREEN + "\nCo chceš udělat?" + RESET)
+    print(YEL + " 1 " + RESET + "- Vypsat seznam kontaktů")
+    print(YEL + " 2 " + RESET + "- Přidat nový kontakt")
+    print(YEL + " 3 " + RESET + "- Najít kontakt")
+    print(YEL + " 4 " + RESET + "- Konec programu")
 
-    volba = input("Zadej číslo volby: ")
+    volba = input(BLUE + "Zadej číslo volby: " + RESET)
 
     if volba == "1":
         vypis_seznam()
